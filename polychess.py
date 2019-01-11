e2ea7273e2f9d4cd4b048f5c840c5e8c49063c7a
RePh Industries <remy.martin@etu.univ-smb.fr>
2019-01-11 10:18:04 +0100

    Save

--- polychess.py
+++ polychess.py
@@ -6,7 +6,9 @@
 #https://github.com/niklasf/python-chess
 import chess
 from minMax import MinMax
+import random
 import time
+import datetime
 from os import listdir
 from os.path import isfile, join
 
@@ -15,18 +17,20 @@ from os.path import isfile, join
 
 def finDuGame (board):
     #do we have a winner?
-    if (board.is_game_over() or board.is_stalemate() or board.is_insufficient_material()):
+    if (board.is_game_over() or board.is_stalemate() or board.is_insufficient_material() or board.is_fivefold_repetition() or board.is_seventyfive_moves()):
         print("")
         print(board)
-        print("The game is over")
-        print(board.result())
-        return False
-    elif (board.is_fivefold_repetition() or board.is_seventyfive_moves()):
-        print("")
-        print(board)
-        print("The game is over because 5 repetitions or 75 moves without capture")
-        print(board.result())
-        return False
+        if (board.is_fivefold_repetition() or board.is_seventyfive_moves()):
+            print("")
+            print(board)
+            print("The game is over because 5 repetitions or 75 moves without capture")
+            print(board.result())
+            return False
+        else:
+            print("GO : ", board.is_game_over(), "\nSTA :", board.is_stalemate(), "\nINS :", board.is_insufficient_material())
+            print("The game is over")
+            print(board.result())
+            return False
     else:
         return True
 
@@ -50,11 +54,62 @@ def mouvementDemande (board):
 #
 #
 
-def teststr(liste_nom):
+def creagame (savefile, PLAYER):
+    path = savefile[1]
+    file = open(path, "w")
+    file.write("[Event \"Polychess > all\"]\n")
+    file.write("[Site \"Polytech Annecy\"]\n")
+    today = datetime.date.today()
+    sy = str(today)
+    a = ""
+    for k in sy:
+        if (k == "-"):
+            a += "."
+        else:
+            a += str(k)
+    file.write("[Date \"" + a + "\"]\n")
+    roun = input("Actual round :\n")
+    file.write("[Round \""+ roun +"\"]\n")
+    if (PLAYER):
+        white = input("Name of the player with the whites :\n")
+        file.write("[White \""+ white +"\"]\n")
+    else:
+        file.write("[White \"PolyBotChess\"]\n")
+    file.write("[Black \"PolychessBot\"]\n")
+    file.write("[Result \"*\"]\n\n")
+    file.write("1.")
+    file.close()
+    
+##
+#
+#
+    
+def saving(fichier, listm):
+    fileLec = open(fichier[1], "r")
+    lec = []
+    for x in fileLec:
+        lec.append(x)
+    fileLec.close()
+    file = open(fichier[1], "w")
+    for i in range (0, 7):
+        file.write(lec[i])
+    file.write("\n")
+    temp = ""
+    for a in listm:
+        temp = temp + " " + a
+    temp = temp[1:]
+    file.write(temp)
+    file.close()
+
+##
+#
+#
+
+def teststr(liste_nom, PLAYER):
     liste_nom += [f for f in listdir("./SAVES/COMPLETED/") if (isfile(join("./SAVES/COMPLETED/", f)) and f.endswith(".pgn"))]
     test=True
     while(test):
-        string=input("name for save with only Number and letters")
+        string=input("Name your save with only numbers and letters (2 to 16) :\n")
         if (not(len(string)>16 or len(string)<2)):
             for k in range (len(string)):
                 if (ord(string[k])<48 or 57<ord(string[k])<65 or 90<ord(string[k])<96 or ord(string[k])>122):
@@ -63,10 +118,17 @@ def teststr(liste_nom):
                 else:
                     test=False
         if (test == False):
-            for n in liste_nom:
-                if n == string + ".pgn":
-                    test=True
-                    break
+            string = string.lower()
+            if (PLAYER):
+                for n in liste_nom:
+                    if n == string + ".pgn":
+                        test = True
+                        break
+            else:
+                for n in liste_nom:
+                    if n == "AI-" + string + ".pgn":
+                        test = True
+                        break
     return (string + ".pgn", "./SAVES/" + string + ".pgn")
 
 ## Main function for the program.
@@ -79,7 +141,6 @@ def main ():
     board = chess.Board()
     #initialiser
     movement = [0, 0]
-    list_moves = []
     NQUIT = True
     smart = 2
     colorAI = 0
@@ -91,9 +152,6 @@ def main ():
         # Make a list of all the .pgn files in the SAVES folder
         allFiles = [f for f in listdir("./SAVES/") if (isfile(join("./SAVES/", f)) and f.endswith(".pgn"))]
         if (len(allFiles) != 0):
-            print ("Here are all saved games : ")
-            for i in range (len(allFiles)):
-                print (i, "-", allFiles[i])
             while (EXISTING != str(0) and EXISTING != str(1)):
                 EXISTING = str(input("Do you want to open an already saved game (1 - yes, 0 - no) ?\n"))
         else:
@@ -102,7 +160,10 @@ def main ():
         if (EXISTING == str(1)):
             EXISTING = str(-1)
             value = -1
-            print ("\n", len(allFiles), "- Retour")
+            print ("Here are all saved games : ")
+            for i in range (len(allFiles)):
+                print (i, "-", allFiles[i])
+            print ("\n", len(allFiles), "- Back to game menu")
             while (value < 0 or value > len(allFiles)):
                 value = str(input("What file do you want to load : \n"))
                 try:
@@ -115,7 +176,10 @@ def main ():
                 # ==========================================================================
                 tempfile = allFiles[value]
                 savefile = (tempfile, "./SAVES/" + tempfile)
-                PLAYER = True;
+                if (savefile[0][0] == "A" and savefile[0][1] == "I" and savefile[0][2] == "-"):
+                    PLAYER = False;
+                else:
+                    PLAYER = True;                
                 # ==========================================================================
                 NOTRETOUR = False
         else:
@@ -127,35 +191,72 @@ def main ():
             jeu = input ("Do you want to play against AI (1) or see an AI play against another AI (2) ?\n")
             if (int(jeu) < 2):
                 PLAYER = True
-            savefile = teststr(allFiles)
+                savefile = teststr(allFiles, PLAYER)
+            else:
+                savefile = teststr(allFiles, PLAYER)
+                savefile = ("AI-" + savefile[0], "./SAVES/AI-" + savefile[0])
+            creagame(savefile, PLAYER)
+        
+    temp = open(savefile[1], "r")
+    for x in temp:
+        if (x[0] == "1" and x[1] == "."):
+            break
+        else:
+            print (x)
+    list_moves = x.split(" ")
+    i = 0
+    if (len(list_moves) > 1):
+        for val in list_moves:
+            print("i :", i, "val :", val)
+            if (i != 0 and val != ""):
+                board.push(chess.Move.from_uci(val))
+            i += 1
+            if (i == 3):
+                i = 0
+    temp.close()
+        
     # ==========================================================================
     # Game gestion (saving game, playing)
     while (finDuGame(board) and NQUIT):
+        turn = list_moves[len(list_moves) - 1][0]
+        print ("Turn", turn)
         # Player versus AI
         if (PLAYER == True):
             movement = mouvementDemande(board)
+            list_moves.append(movement[0])
             board.push(movement[1])
             print("AI turn :")
             print("")
             print(board)
             coup = MinMax(board, smart, colorAI)
+            list_moves.append(str(coup))
             board.push(coup)
 
         # AI versus AI
         else:
             print("")
             print(board)
-            print("White AI turn :")
-            coup = MinMax(board, smart, 1)
-            board.push(coup)
-            print("")
+            print("\nWhite AI turn :")
+            psmov = board.legal_moves
+            numero = random.randint(0, psmov.count()-1)
+            i=0
+            for move in psmov:
+                if i==numero:
+                    list_moves.append(str(move))
+                    board.push(move)
+                    break
+                i+=1
             print(board)
             print("Black AI turn :")
             coup = MinMax(board, smart, 0)
+            list_moves.append(str(coup))
             board.push(coup)
+            
         # Saving the game + quit
         print(board, "\n")
-        print("Game saved in ", savefile[0], savefile[1])
+        list_moves.append(str(int(turn) + 1) + ".")
+        saving(savefile, list_moves)
+        print("Game saved in", savefile[0])
         temp = input("Do you want to quit the game (y or n) ?\n")
         if (temp == "y"):
             print ("You have quitted the game. See you at another time !")
@@ -166,71 +267,3 @@ def main ():
 """
 
 main()
-
-
-#print the board on the console
-"""print(board)
-
-#SVG render for the board is possible in Jupyter Notebook
-#board
-
-#get all the legal moves for the current position
-moves = board.legal_moves
-
-#how many moves are available?
-print(moves.count())
-
-#iterate over all the moves
-for move in moves:
-
-    #display the move
-    print(move)
-
-    #save the current position
-    current_board = board
-
-    #do the move
-    board.push(move)
-
-    #display the board
-    print(board)
-
-    #number of black moves
-    print("Black moves:" + str(board.legal_moves.count()))
-
-    #undo the move
-    board.pop()
-
-    finDuGame(board)"""
-
-"""
-THE TESTS
-"""
-"""
-#set the board to its initial position
-#corresponding to: rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
-board = chess.Board()
-
-
-a = mouvementDemande ()
-print ("a : ", a)
-print(a[0])
-#save the current position
-current_board = board
-#do the move
-board.push(a[1])
-#display the board
-print(board)
-
-print("\nPlayer 2 :")
-
-a = mouvementDemande ()
-print ("a : ", a)
-print(a[0])
-#save the current position
-current_board = board
-#do the move
-board.push(a[1])
-#display the board
-print(board)
-"""
